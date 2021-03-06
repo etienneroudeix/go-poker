33f74dda9972a3c929ea6783e15f04b6b87f5fd9
etienne <er@4x.fr>
2021-03-06 11:23:53 +0100

    :recycle: 5 card analyse

--- model/hand.go
+++ model/hand.go
@@ -43,113 +43,103 @@ func (h *Hand) GetBestHand (board Board) {
 
 func ParseFiveCards (cards [5]Card) ParsedFiveCards {
 	
-	flushValue, flushKickers := testFlush(cards)
-	straightValue, straightKickers := testStraight(cards)
-	
-	if flushValue != 0 && straightValue != 0 {
-		fmt.Println("Has Straight Flush : %d", straightValue)
-		return ParsedFiveCards{RANK_STRAIGHT_FLUSH, straightValue, straightKickers}
-	}
-	
-	value, kickers := testFourOfAKind(cards)
-	if value != 0 {
-		fmt.Println("Has Four Of a Kind : %d", value)
-		return ParsedFiveCards{RANK_FOUR_OF_A_KIND, value, kickers}
-	}
-
-	value, kickers = testFullHouse(cards)
-	if value != 0 {
-		fmt.Println("Has Full House : %d", value)
-		return ParsedFiveCards{RANK_FULL_HOUSE, value, kickers}
-	}
-
-	if flushValue != 0 {
-		fmt.Println("Has Flush : %d", flushValue)
-		return ParsedFiveCards{RANK_FLUSH, flushValue, flushKickers}
-	}
-
-	if straightValue != 0 {
-		fmt.Println("Has Straight : %d", straightValue)
-		return ParsedFiveCards{RANK_STRAIGHT, straightValue, straightKickers}
-	}
-
-	value, kickers = testThreeOfAKind(cards)
-	if value != 0 {
-		fmt.Println("Has Three Of a Kind : %d", value)
-		return ParsedFiveCards{RANK_THREE_OF_KIND, value, kickers}
-	}
-
-	// todo test DP
-	value, kickers = testDoublePair(cards)
-	if value != 0 {
-		fmt.Println("Has Three Of a Kind : %d", value)
-		return ParsedFiveCards{RANK_THREE_OF_KIND, value, kickers}
-	}
-
-	// todo test P
-
-	return ParsedFiveCards{0,0,[]int{}} // TODO test nothing
-}
-
-func testFourOfAKind(cards [5]Card) (int, []int) {
 	stack := make([]int, 13, 13)
 	for _, card := range cards {
 		stack[card.Value - 2]++
 	}
 
-	fokValue := 0
-	kicker := 0
+	pairs := make([]int, 0)
+	fok := 0
+	tok := 0
+	kickers := make([]int, 0)
+
+	hasStraight := true
+	straightTick := 0
+	straightHighCard := 0
 
 	for value, count := range stack {
 		if count == 4 {
-			fokValue = value+2
-			continue
+			fok = value+2;
+			hasStraight = false
 		}
 
-		kicker = value+2
-	}
-
-	return fokValue, []int{kicker}
-}
-
-func testFullHouse(cards [5]Card) (int, []int) {
-	stack := make([]int, 13, 13)
-	for _, card := range cards {
-		stack[card.Value - 2]++
-	}
-
-	tokValue := 0
-	kickers := make([]int, 0)
-
-	for value, count := range stack {
 		if count == 3 {
-			tokValue = value+2
-			continue
+			tok = value+2;
+			hasStraight = false
 		}
 
 		if count == 2 {
-			kickers = append(kickers, value+2, value+2)
+			pairs = append([]int{value+2}, pairs...)
+			hasStraight = false
+		}
+
+		if count == 1 {
+			kickers = append([]int{value+2}, kickers...)
+			
+			straightHighCard = value + 2
+			straightTick++
+		}
+
+		if count == 0 {
+			if straightHighCard != 0 && straightTick != 5 {
+				hasStraight = false
+			}
 		}
 	}
 
-	if tokValue == 0 {
-		return 0, []int{}
+	flushValue := testFlush(cards)
+	
+	if flushValue != 0 && hasStraight {
+		fmt.Println("Has Straight Flush : %d", flushValue, kickers)
+		return ParsedFiveCards{RANK_STRAIGHT_FLUSH, flushValue, kickers}
 	}
 	
-	if len(kickers) != 2 {
-		return 0, []int{}
+	if fok != 0 {
+		fmt.Println("Has Four Of a Kind : %d", fok, kickers)
+		return ParsedFiveCards{RANK_FOUR_OF_A_KIND, fok, kickers}
 	}
 
-	return tokValue, kickers
+	if tok != 0 && len(pairs) == 1 {
+		fmt.Println("Has Full House : %d", tok, []int{pairs[0],pairs[0]})
+		return ParsedFiveCards{RANK_FULL_HOUSE, tok, []int{pairs[0],pairs[0]}}
+	}
+
+	if flushValue != 0 {
+		fmt.Println("Has Flush : %d", flushValue, kickers)
+		return ParsedFiveCards{RANK_FLUSH, flushValue, kickers}
+	}
+
+	if hasStraight {
+		fmt.Println("Has Straight : %d", straightHighCard, kickers)
+		return ParsedFiveCards{RANK_STRAIGHT, straightHighCard, kickers}
+	}
+
+	if tok != 0 {
+		fmt.Println("Has Three Of a Kind : %d", tok, kickers)
+		return ParsedFiveCards{RANK_THREE_OF_KIND, tok, kickers}
+	}
+
+	if len(pairs) == 2 {
+		fmt.Println("Has Two Pairs : %d", pairs[0], []int{pairs[1], kickers[0]})
+		return ParsedFiveCards{RANK_THREE_OF_KIND, pairs[0], []int{pairs[1], kickers[0]}}
+	}
+
+	if len(pairs) == 1 {
+		fmt.Println("Has Pair : %d", pairs[0], kickers)
+		return ParsedFiveCards{RANK_THREE_OF_KIND, pairs[0], kickers}
+	}
+
+	fmt.Println("Has Nothing : %d", kickers[0], kickers)
+	return ParsedFiveCards{RANK_NOTHING, kickers[0], kickers}
 }
 
-func testFlush(cards [5]Card) (int, []int) {
+func testFlush(cards [5]Card) (int) {
 	color := cards[0].Color
 	value := cards[0].Value
 
 	for i:= 1; i<5; i++ {
 		if cards[i].Color != color {
-			return 0, []int{}
+			return 0
 		}
 
 		if cards[i].Value > value {
@@ -157,101 +147,5 @@ func testFlush(cards [5]Card) (int, []int) {
 		}
 	}
 	
-	return value, []int{value}
-}
-
-func testStraight(cards [5]Card) (int, []int) {
-	stack := make([]int, 13, 13)
-	for _, card := range cards {
-		stack[card.Value - 2]++
-	}
-
-	tick := 0
-	highCard := 0
-
-	for value, count := range stack {
-		if count > 1 {
-			return 0, []int{}
-		}
-
-		if count == 0 {
-			if highCard == 0 {
-				continue
-			}
-			return 0, []int{}
-		}
-
-		highCard = value + 2
-		tick++
-
-		if tick == 5 {
-			break
-		}
-	}
-
-	return highCard, []int{}
-}
-
-func testThreeOfAKind(cards [5]Card) (int, []int) {
-	stack := make([]int, 13, 13)
-	for _, card := range cards {
-		stack[card.Value - 2]++
-	}
-
-	tokValue := 0
-	kickers := make([]int, 0)
-
-	for value, count := range stack {
-		if count == 3 {
-			tokValue = value+2
-			continue
-		}
-
-		if count == 2 {
-			// it's a full house
-			return 0, []int{}
-		}
-
-		if count == 1 {
-			kickers = append(kickers, value+2)
-		}
-	}
-
-	if tokValue == 0 {
-		return 0, []int{}
-	}
-
-	if kickers[0] == kickers[1] {
-		// it's a full house
-		return 0, []int{}
-	}
-
-	return tokValue, kickers
-}
-
-func testPairs(cards [5]Card) ([]int, []int) {
-	stack := make([]int, 13, 13)
-	for _, card := range cards {
-		stack[card.Value - 2]++
-	}
-
-	pairs := make([]int, 0)
-	kickers := make([]int, 0)
-
-	for value, count := range stack {
-		if count > 2 {
-			return 0, []int{}
-			continue
-		}
-
-		if count == 2 {
-			pairs = append(pairs, value+2)
-		}
-
-		if count == 1 {
-			kickers = append(kickers, value+2)
-		}
-	}
-
-	return pairs, kickers
+	return value
 }
